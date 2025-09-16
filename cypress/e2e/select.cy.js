b0d3f6cf4d1133b3eeec506eeea9312c6207f105
K4mD4m <KamyszekDamian@outlook.com>
2025-09-16 19:11:44 +0200

    assert on select

--- cypress/e2e/select.cy.js
+++ cypress/e2e/select.cy.js
@@ -4,13 +4,16 @@ describe("Select Test Case", () => {
     it("Click on tab Women", () => {
         cy.visit("/");
         cy.get('a[title="Women"]').click();
+        cy.url().should("include", "id_category=3&controller=category");
     })
 
     it("Chose filtr option Instock", () => {
         cy.get("#selectProductSort").select("In stock")
+        cy.get("#selectProductSort").should("have.value", "quantity:desc")
     })
 
     it("Chose filtr option Product Name: A to Z", () => {
         cy.get("#selectProductSort").select("Product Name: A to Z")
+        cy.get("#selectProductSort").should("have.value", "name:asc")
     })
 })
