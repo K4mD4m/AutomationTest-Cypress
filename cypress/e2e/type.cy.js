1c201f67efda4ccf1b263df1211d995a9ef81f14
K4mD4m <KamyszekDamian@outlook.com>
2025-09-16 19:23:51 +0200

    assert on type

--- cypress/e2e/type.cy.js
+++ cypress/e2e/type.cy.js
@@ -4,9 +4,13 @@ describe("Type text in search box", () => {
     it("Type text", () => {
         cy.visit("/");
         cy.get('#search_query_top').type("Example text");
+        cy.get('#search_query_top').should("have.value", "Example text");
         cy.wait(3000);
         cy.get('#search_query_top').clear();
         cy.wait(3000);
         cy.get('#search_query_top').type("Example text {enter}");
+        cy.get("p.alert").should("be.visible").and("include.text", "No results were found for your search");
+        cy.get('#search_query_top').should("have.class", "search_query");
+        cy.get('#search_query_top').should("have.css", "margin-right", "1px");
     });
 });
