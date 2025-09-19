a404414791e3b504563a551f492e960f6188e9cc
K4mD4m <KamyszekDamian@outlook.com>
2025-09-19 21:32:49 +0200

    check class added (checkbox is checked)

--- cypress/e2e/checkbox.cy.js
+++ cypress/e2e/checkbox.cy.js
@@ -2,6 +2,7 @@
 
 import Base from "../pages/Base";
 import Home from "../pages/Home";
+import Women from "../pages/Women";
 
 describe("Checkbox Test Case", () => {
     it("Click on tab Women", () => {
@@ -11,9 +12,9 @@ describe("Checkbox Test Case", () => {
     })
 
     it("Click on checkbox Women", () => {
-        cy.get("#layered_category_4").check();
-        cy.get("#layered_category_4").should("be.checked");
-        cy.get("#layered_category_8").check();
-        cy.get('#ul_layered_id_attribute_group_1 input').check();
+        Women.checkTops();
+        Women.topsCheckbox.should("be.checked");
+        Women.checkDresses();
+        Women.checkSize();
     })
 })
